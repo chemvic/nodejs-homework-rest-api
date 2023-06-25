75d0caecb6603826b41cb4d99b7f584d5b1100e9
chemvic <chemvic79@gmail.com>
2023-06-25 14:30:11 +0300

    добавил dotenv для env

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const {DB_HOST}=process.env;
 
 mongoose.connect(DB_HOST)
-.then(()=>{console.log("Database connection successful");
-app.listen(3000)})
-.catch(error=>{
-  console.log(error.message);
-  process.exit(1);
+  .then(()=>{console.log("Database connection successful");
+    app.listen(3000)})
+  .catch(error=>{
+    console.log(error.message);
+    process.exit(1);
 });
 
