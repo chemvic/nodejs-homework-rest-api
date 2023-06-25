43257584a1018bd3e96ba79cd675c2b3930d0ed4
chemvic <chemvic79@gmail.com>
2023-06-25 14:12:08 +0300

    DB_host to process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app')
 
 const mongoose = require('mongoose');
 
-const DB_HOST='mongodb+srv://user-for-test:yuzhne@cluster0.11yucsb.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const {DB_HOST}=process.env;
 
 mongoose.connect(DB_HOST)
 .then(()=>{console.log("Database connection successful");
