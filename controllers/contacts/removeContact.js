35ed215b3689dee0e75b0e97c2bcfb0c2ce18232
chemvic <chemvic79@gmail.com>
2023-06-27 16:23:51 +0300

    вынес контроллеры контактов в отдельные файлы

---
+++ controllers/contacts/removeContact.js
@@ -0,0 +1,14 @@
+const { Contact } = require('../../models/contact');
+
+const { ctrlWrapper, HttpError } = require('../../helpers')
+
+const removeContact = async (req, res) => {
+    const {id}=req.params;
+    const result =await Contact.findByIdAndRemove(id);
+    if(!result){
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json({message: "contact deleted"})
+  }  
+
+module.exports = {removeContact: ctrlWrapper(removeContact)};
