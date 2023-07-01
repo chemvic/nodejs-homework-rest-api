6a87205706454d0a4c9b0c79ab18077ce3e68271
chemvic <chemvic79@gmail.com>
2023-07-01 21:32:29 +0300

    добавил в модель id юзера который добавлял контакт в базу

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,8 +2,9 @@ const { Contact } = require('../../models/contact');
 
 const { ctrlWrapper } = require('../../helpers')
 
-const listContacts = async (req, res) => {   
-    const result =await Contact.find();
+const listContacts = async (req, res) => { 
+  const {_id: owner}=req.user;  
+    const result =await Contact.find({owner},'-createdAt -updatedAt').populate('owner', 'name email');
     res.json(result);
   } 
 
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,22 +1,26 @@
 const jwt =require('jsonwebtoken');
 const {HttpError} = require('../helpers');
-const User =require('../models/user/user');
+const User = require('../models/user/user');
 const {SECRET_KEY} = process.env;
 
 const authenticate = async (req, res,next) => {
 
 const {authorization =""} =req.headers;
 const [bearer, token] = authorization.split(" ");
+console.log(token);
 if(bearer!=="Bearer"){
     next(HttpError(401));
 };
 try {
+    console.log("до візова jwt.verify");
     const {id} =jwt.verify(token, SECRET_KEY);
- 
+  console.log("после візова jwt.verify");
     const user = await User.findById(id);
+    console.log(`user.id ${user.id}`);
     if(!user){
         next(HttpError(401));
-    }
+    };
+    req.user = user;
     next();
 
 } catch  {
