9b68c0a4efef6b0d60e0b774493c0d23c77aa356
chemvic <chemvic79@gmail.com>
2023-07-02 23:00:39 +0300

    update

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,10 +1,10 @@
 const { ctrlWrapper} = require('../../helpers');
 
 const getCurrent = async (req, res) => {
-    const {email, name} = req.user;
+    const {email, subscription} = req.user;
     res.json({
     email,
-    name})
+    subscription})
 
 };
 
--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -5,7 +5,7 @@ const { ctrlWrapper } = require('../../helpers');
 const logout = async (req,res) => {
     const {_id} =req.user;
     await User.findByIdAndUpdate(_id, {token: ''});
-    res.json({
+    res.status(204).json({
         message: 'Logout successful'
     })
 
