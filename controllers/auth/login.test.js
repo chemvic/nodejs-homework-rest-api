3be05be0e954f12c25812fa6461c8e3c5299d9f8
chemvic <chemvic79@gmail.com>
2023-07-11 15:49:49 +0300

    test

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -1,26 +1,46 @@
-const  User  = require('../../models/user');
-const bcrypt =require('bcrypt');
-const jwt = require('jsonwebtoken');
+// const  User  = require('../../models/user');
+// const bcrypt =require('bcrypt');
+// const jwt = require('jsonwebtoken');
 
-const { ctrlWrapper, HttpError} = require('../../helpers');
-const {SECRET_KEY}=process.env;
 
-const express = require('express');
+// const express = require('express');
 const  request = require('supertest');
+
 const {login} = require('./login');
 
-const app = express();
-// const server =app.listen(3000);
-app.post('./login', login);
+// const app = express();
+// app.use(express.json())
+const app = require('../../app');
+app.post('/api/auth/login', login);
 
-describe("test login controller", ()=>{
-    beforeAll(()=>app.listen(3000));
-    // afterAll(()=>app.close());
+describe('login controller', () => {
+  // let server;
+  //   beforeAll(()=> server = app.listen(3000));
+  //   afterAll(()=> server.close());
 
-    test("response with status 200", async ()=>{
-      const response = await request(app).post('/login', {email:"qwer@od.co", password: "111111"});
-      expect(response.status).toBe(200);
-    })
+ 
 
-})
+  test('should respond with status 200 and token', async () => {
+    await request(app)
+      .post('/api/auth/login')
+      .send({  email: 'qwer@od.co', password: '111111'  });
+
+    expect(200);
+    // expect(response.body).toHaveProperty('token');
+  }, 10000);
+
+});
+
+// describe("test login controller", ()=>{
+//   let server;
+//   beforeAll(()=> server = app.listen(3000));
+//   afterAll(()=> server.close());
+
+//     test("response with status 200", async ()=>{
+//       const response = await request(app).post('/login')
+//       .send({ email: 'qwer@od.co', password: '111111' });
+//       expect(response.status).toBe(200);
+//     }, 20000);
+
+// })
 
---
+++ helpers/sendEmail.js
@@ -0,0 +1,9 @@
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+
+const {SENDGRID_API_KEY} = process.env;
+
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+
+const sgMail = ()
