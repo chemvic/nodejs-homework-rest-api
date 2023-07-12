86d337a7de3bc18875e2b2ae3c96cf30198bd9a4
chemvic <chemvic79@gmail.com>
2023-07-12 16:43:17 +0300

    перенес SECRET_KEY в login для нормальной работы теста login

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -1,19 +1,58 @@
 const  request = require('supertest');
-
+const mongoose = require('mongoose');
 const {login} = require('./login');
-
 const app = require('../../app');
-app.post('/api/auth/login', login);
+const { DB_HOST }=process.env;
 
-describe ('login controller', () => {
+mongoose.set('strictQuery', true);
+app.post('/api/users/login', login);
 
-  test ('response with status 200', async () => {
+describe ('login controller', function() {
+
+beforeAll( async() => {
+
+
+ await mongoose.connect(DB_HOST)
+    .then(()=>{ 
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+    .catch(error=>{
+      console.log(error.message);
+      process.exit(1);
+  });
+});
+
+
+afterAll( async() => {
+await mongoose.disconnect(DB_HOST);
+console.log(" DB disconnected");
+});
+
+  test ('response with status 200 and token', async () => {    
+    
     const response = await request(app)
-      .post('/api/auth/login')
-      .send({  email: 'qwer@od.co', password: '111111'  });
-      
+      .post('/api/users/login')
+      .send({  email: 'qwer@od.co', password: '111111'  })
+          
        expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('token');
+       expect(response.body).toHaveProperty('token');
+
+  });
+  test ('response with object user includes email and subscription Strings', async () => {
+    
+    
+    const response = await request(app)
+      .post('/api/users/login')
+      .send({  email: 'qwer@od.co', password: '111111'  })
+            
+       expect(response.body).toMatchObject(
+        {user:
+          {
+            email: expect.any(String),
+            subscription: expect.any(String)
+          }
+        });  
   });
 
 });
